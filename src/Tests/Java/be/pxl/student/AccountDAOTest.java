a8322ab20acac2e01e3982b1bba4cb1cb88950f9
MaartenVanhaerenPXL <11800644@student.pxl.be>
2020-03-04 16:16:30 +0100

    DAO Aaccount en test DAO Account af

---
+++ src/Tests/Java/be/pxl/student/AccountDAOTest.java
@@ -0,0 +1,24 @@
+package be.pxl.student;
+
+import be.pxl.student.DAO.AccountDAO;
+import be.pxl.student.entity.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class AccountDAOTest {
+
+    @Test
+    public void testAccountInsert(){
+        Account account = new Account();
+        account.setIBAN("testIBAN");
+        account.setName("testName");
+
+        AccountDAO accountDAO=new AccountDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
+
+        Account accountInserted = accountDAO.createAccount(account);
+        //assertEquals(account,accountInserted);
+        assertNotEquals(0,accountInserted.getId());
+        System.out.println(accountInserted.getId());
+    }
+}
---
+++ src/main/java/be/pxl/student/DAO/PaymentDAO.java
@@ -0,0 +1,4 @@
+package be.pxl.student.DAO;
+
+public class PaymentDAO {
+}
