36ee8efdb455ac623f007b8cffb7070716f6330c
MaartenVanhaerenPXL <11800644@student.pxl.be>
2020-03-11 14:38:22 +0100

    Test accountDAO Read af

--- src/tests/java/be/pxl/student/dao/AccountDAOTest.java
+++ src/tests/java/be/pxl/student/dao/AccountDAOTest.java
@@ -4,21 +4,46 @@ import be.pxl.student.entity.Account;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccountDAOTest {
+    private AccountDAO accountDAO=new AccountDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
+    private Account account = new Account();
 
     @Test
     public void testAccountInsert(){
-        Account account = new Account();
         account.setIBAN("testIBAN");
         account.setName("testName");
-
-        AccountDAO accountDAO=new AccountDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
 
         Account accountInserted = accountDAO.createAccount(account);
         //assertEquals(account,accountInserted);
         assertNotEquals(0,accountInserted.getId());
         System.out.println(accountInserted.getId());
+    }
+
+    @Test
+    public void testAccountUpdate(){
+        account.setIBAN("testIBAN");
+        account.setName("testNameAangepast");
+        account.setId(2);
+
+        boolean uitgevoerd = accountDAO.updateAccount(account);
+        assertTrue(uitgevoerd);
+        //assert
+        System.out.println(uitgevoerd);
+
+    }
+    @Test
+    public void testAccountDelete(){
+
+
+    }
+    @Test
+    public void testAccountRead(){
+        Account accountRead = accountDAO.readAccount(1);
+        assertNotEquals(null, accountRead);
+        System.out.println("name: " + accountRead.getName());
+        System.out.println("IBAN: " + accountRead.getIBAN());
     }
 
 }
--- src/tests/java/be/pxl/student/dao/PaymentDAOTest.java
+++ src/tests/java/be/pxl/student/dao/PaymentDAOTest.java
@@ -1,4 +1,29 @@
-package be.pxl.student.DAO;
+package be.pxl.student.dao;
+
+import be.pxl.student.entity.Payment;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PaymentDAOTest {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+    @Test
+    public void testPaymentInsert(){
+        Payment payment = new Payment();
+        payment.setDate(LocalDateTime.parse("Wed Feb 26 06:41:27 CET 2020", formatter));
+        payment.setAmount(1000);
+        payment.setCurrency("EUR");
+        payment.setDetail("Detail");
+
+        PaymentDAO paymentDAO=new PaymentDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
+
+        Payment paymentInserted = paymentDAO.createPayment(payment);
+        //assertEquals(account,accountInserted);
+        assertNotEquals(0,paymentInserted.getId());
+        System.out.println(paymentInserted.getId());
+    }
 }
